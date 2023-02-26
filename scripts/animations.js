cceaa01d490149dc2b1943d838908ffa131caa19
ivanSpi <ivannotnull@gmail.com>
2023-02-26 13:44:00 +0300

    first animations

---
+++ scripts/animations.js
@@ -0,0 +1,6 @@
+
+document.getElementById('square1').style.animation = 'spinmove 8s linear infinite'
+document.getElementById('square2').style.animation = 'spinmove 15s linear infinite'
+
+let square = document.getElementById('square3')
+square.style.animation =  'bouncing 5s linear infinite alternate-reverse'
